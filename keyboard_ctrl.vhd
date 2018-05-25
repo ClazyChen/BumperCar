ff873356529119b8377ff002f6b4edd7400f6861
shinbokuow <wyfcyx.gmail.com>
2018-05-25 19:56:55 +0800

    Completed, you can find I/O signal configuration in keyboard_ctrl.vhd. Find some key conflicting bugs, but we can ignore them.

--- keyboard_ctrl.vhd
+++ keyboard_ctrl.vhd
@@ -1,24 +1,20 @@
 ------------------------------------------
- -- 键盘控制模块
- -- 创建日期：2018-5-19
+ -- 键盘监测模块
+ -- 创建日期：2018-5-25
  -- 负责人：吴一凡
- -- 信号说明：
- --
 ------------------------------------------
-
 library ieee;
+
 use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
 entity keyboard_ctrl is
 	port(
-		--输入的ps2键盘的时钟与数据
-		ps2_data_in, ps2_clk_in : in std_logic;
-		--输入的100MHz时钟以及reset信号
-		clk, rst : in std_logic;
-
-		--输出当前W,A,S,D,UP,LEFT,DOWN,RIGHT这8个键目前是否被按下
+		--输入信号：ps2键盘时钟ps2_clk_in，ps2键盘数据ps2_data_in，100M时钟clk100m
+		ps2_clk_in, ps2_data_in : in std_logic;
+		clk100m : in std_logic;
+		--输出信号：一个长度为8的向量key_pressing_vector，分别表示W,S,A,D,UP,LEFT,DOWN,RIGHT当前是否按下
 		key_pressing_vector : out std_logic_vector(7 downto 0)
 	);
 end keyboard_ctrl;
@@ -26,98 +22,107 @@ end keyboard_ctrl;
 architecture implementation of keyboard_ctrl is
 component ps2_keyboard is
 	port(
-		ps2_data_in, ps2_clk_in, clk, rst: in std_logic;
-		allow : out std_logic;
-		scancode : out std_logic_vector(7 downto 0)
+		ps2_clk_in, ps2_data_in, clk100m : in std_logic;
+		scancode : out std_logic_vector(7 downto 0);
+		allow : out std_logic
 	);
 end component;
 signal allow : std_logic;
 signal scancode : std_logic_vector(7 downto 0);
-signal key_release : std_logic;
+--E0是否已经出现
+signal E0 : std_logic := '0';
+--F0是否已经出现
+signal F0 : std_logic := '0';
 begin
 	ps2_keyboard_realization : ps2_keyboard port map(
-		ps2_data_in => ps2_data_in,
 		ps2_clk_in => ps2_clk_in,
-		clk => clk,
-		rst => rst,
-		allow => allow,
-		scancode =>scancode);
-	process (clk)
+		ps2_data_in => ps2_data_in,
+		clk100m => clk100m,
+		scancode => scancode,
+		allow => allow);
+
+	process (allow, E0, F0)
 	begin
-		if rising_edge(clk) then
-			if (allow = '1') then
-				case (scancode) is
-					--release, F0
-					when "00001111" =>
-						key_release <= '1';
-					--W, 1D
-					when "10111000" =>
-						if (key_release = '1') then
-							key_pressing_vector(7) <= '0';
-							key_release <= '0';
-						else
-							key_pressing_vector(7) <= '1';
-						end if;
-					--A, 1C
-					when "00111000" =>
-						if (key_release = '1') then
-							key_pressing_vector(6) <= '0';
-							key_release <= '0';
-						else
-							key_pressing_vector(6) <= '1';
-						end if;
-					--S, 1B
-					when "11011000" =>
-						if (key_release = '1') then
-							key_pressing_vector(5) <= '0';
-							key_release <= '0';
-						else
-							key_pressing_vector(5) <= '1';
-						end if;
-					--D, 23
-					when "11000100" =>
-						if (key_release = '1') then
-							key_pressing_vector(4) <= '0';
-							key_release <= '0';
-						else
-							key_pressing_vector(4) <= '1';
-						end if;
-					--UP, 75
-					when "10101110" =>
-						if (key_release = '1') then
-							key_pressing_vector(3) <= '0';
-							key_release <= '0';
-						else
-							key_pressing_vector(3) <= '1';
-						end if;
-					--LEFT, 6B
-					when "11010110" =>
-						if (key_release = '1') then
-							key_pressing_vector(2) <= '0';
-							key_release <= '0';
-						else
-							key_pressing_vector(2) <= '1';
-						end if;
-					--DOWN, 72
-					when "01001110" =>
-						if (key_release = '1') then
-							key_pressing_vector(1) <= '0';
-							key_release <= '0';
-						else
-							key_pressing_vector(1) <= '1';
-						end if;
-					--RIGHT, 74
-					when "00101110" =>
-						if (key_release = '1') then
-							key_pressing_vector(0) <= '0';
-							key_release <= '0';
-						else
-							key_pressing_vector(0) <= '1';
-						end if;
-					--Others
-					when others =>
-				end case;
-			end if;
+		if (allow'event and allow = '1') then
+			case scancode is
+				when "11100000" =>
+					E0 <= '1';
+				when "11110000" =>
+					F0 <= '1';
+				--W,1D/F01D
+				when "00011101" =>
+					if (F0 = '1') then
+						key_pressing_vector(7) <= '0';
+						F0 <= '0';
+					else
+						key_pressing_vector(7) <= '1';
+					end if;
+				--A,1C/F01C
+				when "00011100" =>
+					if (F0 = '1') then
+						key_pressing_vector(6) <= '0';
+						F0 <= '0';
+					else
+						key_pressing_vector(6) <= '1';
+					end if;
+				--S,1B/F01B
+				when "00011011" =>
+					if (F0 = '1') then
+						key_pressing_vector(5) <= '0';
+						F0 <= '0';
+					else
+						key_pressing_vector(5) <= '1';
+					end if;
+				--D,23/F023
+				when "00100011" =>
+					if (F0 = '1') then
+						key_pressing_vector(4) <= '0';
+						F0 <= '0';
+					else
+						key_pressing_vector(4) <= '1';
+					end if;
+				--UP,E075/E0F075
+				when "01110101" =>
+					if ((E0 and F0) = '1') then
+						key_pressing_vector(3) <= '0';
+						E0 <= '0';
+						F0 <= '0';
+					elsif (E0 = '1') then
+						key_pressing_vector(3) <= '1';
+						E0 <= '0';
+					end if;
+				--LEFT,E06B/E0F06B
+				when "01101011" =>
+					if ((E0 and F0) = '1') then
+						key_pressing_vector(2) <= '0';
+						E0 <= '0';
+						F0 <= '0';
+					elsif (E0 = '1') then
+						key_pressing_vector(2) <= '1';
+						E0 <= '0';
+					end if;
+				--DOWN,E072/E0F072
+				when "01110010" =>
+					if ((E0 and F0) = '1') then
+						key_pressing_vector(1) <= '0';
+						E0 <= '0';
+						F0 <= '0';
+					elsif (E0 = '1') then
+						key_pressing_vector(1) <= '1';
+						E0 <= '0';
+					end if;
+				--RIGHT,E074/E0F074
+				when "01110100" =>
+					if ((E0 and F0) = '1') then
+						key_pressing_vector(0) <= '0';
+						E0 <= '0';
+						F0 <= '0';
+					elsif (E0 = '1') then
+						key_pressing_vector(0) <= '1';
+						E0 <= '0';
+					end if;
+				when others =>
+			end case;
 		end if;
 	end process;
 end implementation;
--- ps2_keyboard.vhd
+++ ps2_keyboard.vhd
@@ -1,135 +1,112 @@
 ------------------------------------------
- -- ps2输入-扫描码转换模块
- -- 创建日期：2018-5-19
+ -- ps2数据接收模块
+ -- 创建日期：2018-5-25
  -- 负责人：吴一凡
- -- 信号说明：
- --
 ------------------------------------------
-
 library ieee;
+
 use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
 entity ps2_keyboard is
 	port(
-		--ps2 input data and clk
-		ps2_data_in, ps2_clk_in: in std_logic;
-		--100MHz clk input
-		clk : in std_logic;
-		--rst input
-		rst : in std_logic;
-
-		--allow signal
-		allow : out std_logic;
-		--output scancode
-		scancode : out std_logic_vector(7 downto 0)
+		ps2_clk_in : in std_logic;
+		ps2_data_in : in std_logic;
+		clk100m : in std_logic;
+		scancode : out std_logic_vector(7 downto 0);
+		allow : out std_logic
 	);
 end ps2_keyboard;
 
 architecture implementation of ps2_keyboard is
-type scan_state is (delay, start, d0, d1, d2, d3, d4, d5, d6, d7, parity, stop, finish);
-signal cur : scan_state;
+--滤波后的ps2时钟、数据
+signal ps2_clk, ps2_data : std_logic;
+--5M的滤波时钟
+signal clk5m : std_logic := '0';
+signal count : std_logic_vector(4 downto 0) := "00000";
+constant num : std_logic_vector(4 downto 0) := "10101";
+--状态机
+type state is (start, d0, d1, d2, d3, d4, d5, d6, d7, parity, stop, finish);
+signal cur : state := start;
+--自己算出的奇偶校验码
+signal odd : std_logic;
+--将之前的扫描码存储下来
 signal code : std_logic_vector(7 downto 0);
-signal ps2_data, ps2_clk, clk1, clk2, odd, allow_t : std_logic;
 begin
-	--对信号进行滤波处理
-	clk1 <= ps2_clk_in when rising_edge(clk);
-	clk2 <= clk1 when rising_edge(clk);
-	ps2_clk <= (not clk1) and clk2;
-	
-	ps2_data <= ps2_data_in when rising_edge(clk);
-	--计算奇偶校验码
-	odd <= code(0) xor code(1) xor code(2) xor code(3) xor code(4) xor code(5) xor code(6) xor code(7);	
-	
-	--输出扫描码
-	scancode <= code when allow_t = '1';
-	allow <= allow_t;
-
-	process (clk, rst)
+	--生成5M滤波时钟
+	process (clk100m)
 	begin
-		if (rst = '1') then
-			cur <= delay;
-			allow_t <= '0';
-			code <= (others => '0');
-		elsif rising_edge(clk) then
-			allow_t <= '0';
+		if (clk100m'event and clk100m = '1') then
+			count <= count + 1;
+			if (count = num) then
+				count <= (others => '0');
+				clk5m <= not clk5m;
+			end if;
+		end if;
+	end process;
+	--利用5M时钟对于ps2时钟、数据进行滤波
+	process (clk5m)
+	begin
+		if (clk5m'event and clk5m = '1') then
+			ps2_clk <= ps2_clk_in;
+			ps2_data <= ps2_data_in;
+		end if;
+	end process;
+
+	--计算校验码
+	odd <= code(0) xor code(1) xor code(2) xor code(3) xor code(4) xor code(5) xor code(6) xor code(7);
+
+	process (ps2_clk, ps2_data, cur)
+	begin
+		--键盘时钟遇到负边沿,对于数据进行处理
+		if (ps2_clk'event and ps2_clk = '0') then
 			case (cur) is
-				when delay =>
-					cur <= start;
 				when start =>
-					if (ps2_clk = '1') then
-						if (ps2_data = '1') then
-							cur <= d0;
-						else
-							cur <= delay;
-						end if;
+					if (ps2_data = '0') then
+						cur <= d0;
+						allow <= '0';
 					end if;
 				when d0 =>
-					if (ps2_clk = '1') then
-						code(0) <= ps2_data;
-						cur <= d1;
-					end if;
+					code(0) <= ps2_data;
+					cur <= d1;
 				when d1 =>
-					if (ps2_clk = '1') then
-						code(1) <= ps2_data;
-						cur <= d2;
-					end if;
+					code(1) <= ps2_data;
+					cur <= d2;
 				when d2 =>
-					if (ps2_clk = '1') then
-						code(2) <= ps2_data;
-						cur <= d3;
-					end if;
+					code(2) <= ps2_data;
+					cur <= d3;
 				when d3 =>
-					if (ps2_clk = '1') then
-						code(3) <= ps2_data;
-						cur <= d4;
-					end if;
+					code(3) <= ps2_data;
+					cur <= d4;
 				when d4 =>
-					if (ps2_clk = '1') then
-						code(4) <= ps2_data;
-						cur <= d5;
-					end if;
+					code(4) <= ps2_data;
+					cur <= d5;
 				when d5 =>
-					if (ps2_clk = '1') then
-						code(5) <= ps2_data;
-						cur <= d6;
-					end if;
+					code(5) <= ps2_data;
+					cur <= d6;
 				when d6 =>
-					if (ps2_clk = '1') then
-						code(6) <= ps2_data;
-						cur <= d7;
-					end if;
+					code(6) <= ps2_data;
+					cur <= d7;
 				when d7 =>
-					if (ps2_clk = '1') then
-						code(7) <= ps2_data;
-						cur <= parity;
-					end if;
+					code(7) <= ps2_data;
+					cur <= parity;
 				when parity =>
-					if (ps2_clk = '1') then
-						if ((odd xor ps2_data) = '1') then
-							cur <= stop;
-						else
-							cur <= delay;
-						end if;
-					end if;
-				when stop =>
-					if (ps2_clk = '1') then
-						if (ps2_data = '0') then
-							cur <= finish;
-						else
-							cur <= delay;
-						end if;
+					if ((ps2_data xor odd) = '1') then
+						cur <= finish;
+					else
+						cur <= start;
 					end if;
 				when finish =>
-					if (ps2_clk = '1') then
-						allow_t <= '1';
-						cur <= delay;
+					if (ps2_data = '1') then
+						allow <= '1';
+						scancode <= code;
+						cur <= start;
+					else
+						cur <= start;
 					end if;
 				when others =>
 			end case;
 		end if;
 	end process;
-
 end implementation;
-
